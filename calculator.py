796e7d7ce94bf1681e550c5d66e14924a13dedeb
MANISH BHATI <bhatimanish06082005@gmail.com>
2025-09-18 22:53:44 +0530

    Describe your changes here, e.g., fixed division bug

--- calculator.py
+++ calculator.py
@@ -1,39 +1,33 @@
 try:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
 
-    num1=int(input("etnter the first number"))
-    num2=int(input("etnter the second number"))
+    print("CHOOSE OPERATION TO PERFORM:\n"
+          "1. ADDITION\n"
+          "2. SUBTRACTION\n"
+          "3. DIVISION\n"
+          "4. MULTIPLICATION\n")
 
-    print("CHOOSE OPERATION TO PERFORM: \n",
-          "1. ADDITION\n",
-          "2. SUBTRACTION\n",
-          "3. DIVISION\n",
-          "4. MULTIPLICATION\n"
-                          )
-    
-    cal=input().strip().lower()
-    
-    if cal=="ADDITION":
-        ans=num1+num2
-    elif cal=="SUBTRACTION":
-        ans=num1-num2
-    elif cal=="DIVISION":
-        if num2==0:
-            print("not define:")
+    cal = input("Enter operation name (e.g., ADDITION): ").strip().upper()
+
+    if cal == "ADDITION":
+        ans = num1 + num2
+    elif cal == "SUBTRACTION":
+        ans = num1 - num2
+    elif cal == "DIVISION":
+        if num2 == 0:
+            print("Cannot divide by zero!")
+            ans = None
         else:
-            ans=num1/num2    
-    elif cal=="MULTIPLICATION":
-        ans=num1*num2  
-
+            ans = num1 / num2
+    elif cal == "MULTIPLICATION":
+        ans = num1 * num2
     else:
-        print("invalid number")
-
-        ans=None
+        print("Invalid operation!")
+        ans = None
 
     if ans is not None:
+        print(f"The answer is: {ans}")
 
-       print(f"the answer is: {ans}") 
-
-except ValueErrors:
-
-
-    print("INVALID INPUT!,please enter valid numbers:")
+except ValueError:
+    print("INVALID INPUT! Please enter valid numbers.")
